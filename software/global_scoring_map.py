cbc6f92780f6f3c7eb9f2c32c8e4df4b55f0b1f6
knorwood0 <56272255+knorwood0@users.noreply.github.com>
2022-08-03 13:38:41 +0200

    added score based on lineage frequency in current month

--- software/global_scoring_map.py
+++ software/global_scoring_map.py
@@ -5,34 +5,32 @@ import sys
 import pandas as pd
 import plotly.express as px
 
-df = pd.read_csv(sys.argv[0], sep = "\t")
-output = sys.argv[1]
+df = pd.read_csv(sys.argv[1], sep = "\t")
+output = sys.argv[2]
+month_file = sys.argv[3]
 
-# Keeping variants that have an average mutation score greater than the assigned VOC threshold
-df_vis_threshold = df_vis[df_vis["antigenic_score"] > 2.67]
-df_vis_threshold['averaged_antigenic_score_per_country'] = df_vis_threshold.groupby('Country')['antigenic_score'].transform('mean')
-df_vis_threshold_averaged = df_vis_threshold[['Continent','Country', 'averaged_antigenic_score_per_country']]
-df_vis_threshold_averaged = df_vis_threshold_averaged.drop_duplicates()
-df_vis_threshold_averaged
+# Reading in current month
+month_file.seek(0)
+month = month_file.readline()
+month_file.close()
 
 # European only df for focused visualization:
-df_vis_threshold_averaged_eu = df_vis_threshold_averaged[df_vis_threshold_averaged['Continent'] == "Europe"]
-df_vis_threshold_averaged_eu
+df_eu = df[df['Continent'] == "Europe"]
 
 # Creating global and european map of averaged antigenic scores
-labels_dict = dict(zip(df_vis_threshold_averaged.Country, df_vis_threshold_averaged.averaged_antigenic_score_per_country))
-fig = px.choropleth(df_vis_threshold_averaged, locations = "Country",
+labels_dict = dict(zip(df.Country, df.country_score))
+fig = px.choropleth(df, locations = "Country",
                            locationmode = "country names",
-                           color = "averaged_antigenic_score_per_country",
+                           color = "country_score",
                            color_continuous_scale = 'spectral_r',
                            labels = labels_dict,
-                           title = 'Antigenic Scores per Country')
+                           title = 'Antigenic Scores per Country for %s' %month)
 fig.write_html(output + 'antigenic_score_map.html')
-fig_eu = px.choropleth(df_vis_threshold_averaged_eu, locations = "Country",
+fig_eu = px.choropleth(df_eu, locations = "Country",
                            locationmode = "country names",
-                           color = "averaged_antigenic_score_per_country",
+                           color = "country_score",
                            color_continuous_scale = 'spectral_r',
                            labels = labels_dict,
-                           title = 'Antigenic Scores per Country',
+                           title = 'Antigenic Scores per Country for %s' %month,
                            scope = 'europe')
 fig_eu.write_html(output + 'antigenic_score_map_europe.html')
