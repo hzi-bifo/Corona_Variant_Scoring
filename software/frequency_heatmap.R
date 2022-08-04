0078ecf4ab62eee05b02d45e6527627d1d36df05
knorwood0 <56272255+knorwood0@users.noreply.github.com>
2022-08-04 12:01:29 +0200

    added output directory option

--- software/frequency_heatmap.R
+++ software/frequency_heatmap.R
@@ -12,11 +12,22 @@ folder <- args[1]
 input <- read.csv(args[2], sep = "\t")
 months_file <- args[3]
 cutoff <- as.numeric(args[4])
+output <- args[5]
 
-get_heatmap <- function(folder, countries, current_month, output_name, rename, cutoff, lineages){ #Can add heatmap here
+print("Folder")
+print(folder)
+#print(input)
+print("Months_file")
+print(months_file)
+print("Cutoff")
+print(cutoff)
+print("Output")
+print(output)
+
+get_heatmap <- function(folder, countries, current_month, output_name, rename, cutoff, lineages, output){
   
   # get significant lineages from all countries
-  sign_lineages_df <- lineages[lineages[['antigenic_score']] >= 2.67, ] # 2.67
+  sign_lineages_df <- lineages[lineages[['antigenic_score']] >= 2.67, ]
   sign_lineages <- sign_lineages_df[['Pango.lineage']]
   # remove duplicates
   sign_lineages <- sort(unique(sign_lineages))
@@ -42,7 +53,7 @@ get_heatmap <- function(folder, countries, current_month, output_name, rename, c
   data <- data[rowSums(data) > 0,,drop=FALSE]
   
   # write table
-  write.csv(data, paste(folder, "/", output_name, "_statistics.csv", sep = ""), quote = FALSE)
+  write.csv(data, paste(output, "/", output_name, "_statistics.csv", sep = ""), quote = FALSE)
   
   # further filtering using the cutoff for plotting
   data <- data[rowSums(data) >= cutoff,,drop=FALSE]
@@ -55,7 +66,7 @@ get_heatmap <- function(folder, countries, current_month, output_name, rename, c
   }
   data <- data[order(rowSums(data), decreasing = TRUE),]
   heatmap <- d3heatmap(data, Rowv = NULL, Colv = NULL, colors = "Reds", xaxis_font_size = "12pt", yaxis_font_size = "12pt", xaxis_height = 120)
-  saveWidget(heatmap, paste(folder, "/", output_name, "_heatmap.html", sep = ""), selfcontained = TRUE)
+  saveWidget(heatmap, paste(output, "/", output_name, "_heatmap.html", sep = ""), selfcontained = TRUE)
 }
 
 # get all countries to be analyzed based on the existing folders
@@ -66,5 +77,6 @@ states <- dir(path = folder, pattern = "^DE_") # only German states
 months <- as.vector(t(read.table(months_file, stringsAsFactors = FALSE)))
 current_month <- months[length(months)-1] # look at frequencies of the previous month to get representative values
 
-get_heatmap(folder, countries, current_month, "antigenic_scoring_summary", TRUE, cutoff, input)
-get_heatmap(folder, states, current_month, "antigenic_scoring_summary_states", FALSE, cutoff, input)
+get_heatmap(folder, countries, current_month, "antigenic_scoring_summary", TRUE, cutoff, input, output)
+#get_heatmap(folder, countries, current_month, "antigenic_scoring_summary", FALSE, cutoff, input)
+get_heatmap(folder, states, current_month, "antigenic_scoring_summary_states", FALSE, cutoff, input, output)
