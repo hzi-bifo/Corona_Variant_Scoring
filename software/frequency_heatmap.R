af907cd3720d7039c84a68ba42d927408170fc7b
knorwood0 <56272255+knorwood0@users.noreply.github.com>
2023-03-28 13:09:06 +0200

    Updated heatmap to show only top 20 pVOIs and output df for plotly visual

--- software/frequency_heatmap.R
+++ software/frequency_heatmap.R
@@ -1,11 +1,13 @@
 #!/usr/bin/env Rscript
 
+#install.packages("devtools")
 #library(devtools)
 #install_version("d3heatmap", version = "0.6.1.2")
 
 library(d3heatmap)
 library(htmlwidgets)
 library(countrycode)
+library(DT)
 
 args = commandArgs(trailingOnly=TRUE)
 folder <- args[1]
@@ -28,9 +30,9 @@ get_heatmap <- function(folder, countries, current_month, output_name, rename, c
   
   # get significant lineages from all countries
   sign_lineages_df <- lineages[lineages[['antigenic_score']] >= 3.85, ]
-  sign_lineages <- sign_lineages_df[['Pango.lineage']]
   # remove duplicates
-  sign_lineages <- sort(unique(sign_lineages))
+  sign_lineages_df[!duplicated(sign_lineages_df), ]
+  sign_lineages <- sign_lineages_df[['Pango.lineage']]
   
   # get data
   data <- matrix(data = 0, nrow = length(sign_lineages), ncol = length(countries))
@@ -57,14 +59,33 @@ get_heatmap <- function(folder, countries, current_month, output_name, rename, c
   
   # further filtering using the cutoff for plotting
   data <- data[rowSums(data) >= cutoff,,drop=FALSE]
+  # keeping only the top 20 lineages for visualization
+  data <- data[1:20,]
   
+  # saving dataframe with identified pVOIs and their antigenic scores for later visualization
+  pvois <- row.names(data)
+  pvoi_df <- sign_lineages_df[sign_lineages_df$Pango.lineage %in% pvois, ]
+  # adding frequency data 
+  freq_df <- data
+  global_freq <- rowSums(freq_df)
+  freq_df <- cbind(freq_df, global_freq)
+  freq_df_subset = subset(freq_df, select = c(global_freq))
+  Pango.lineage <- row.names(freq_df_subset)
+  freq_df_subset <- cbind(freq_df_subset, Pango.lineage)
+  # binding to significant lineages df
+  pvoi_df_final <- pvoi_df[c("Pango.lineage", "antigenic_score")]
+  pvoi_final <- merge(pvoi_df_final, freq_df_subset, by=c("Pango.lineage"))
+  #ordering by antigenic score
+  pvoi_final <- pvoi_final[order(pvoi_final$antigenic_score, decreasing = TRUE),]  
+  write.csv(pvoi_final, paste(output, "/", output_name, "_pVOI_table.csv", sep = ""), quote = FALSE, row.names = FALSE)
+
   # plot heatmap and save as html
   if (rename == TRUE){
     country_names <- countrycode(colnames(data), origin = 'iso2c', destination = 'country.name')
     country_names[country_names == "Hong Kong SAR China"] <- "Hong Kong" # rename Hong Kong to make it shorter
     colnames(data) <- country_names
   }
-  data <- data[order(rowSums(data), decreasing = TRUE),]
+  #data <- data[order(rowSums(data), decreasing = TRUE),]
   heatmap <- d3heatmap(data, Rowv = NULL, Colv = NULL, colors = "Reds", xaxis_font_size = "12pt", yaxis_font_size = "12pt", xaxis_height = 120)
   saveWidget(heatmap, paste(output, "/", output_name, "_heatmap.html", sep = ""), selfcontained = TRUE)
 }
@@ -80,3 +101,19 @@ current_month <- months[length(months)-1] # look at frequencies of the previous 
 get_heatmap(folder, countries, current_month, "antigenic_scoring_summary", TRUE, cutoff, input, output)
 #get_heatmap(folder, countries, current_month, "antigenic_scoring_summary", FALSE, cutoff, input)
 get_heatmap(folder, states, current_month, "antigenic_scoring_summary_states", FALSE, cutoff, input, output)
+
+# saving dataframe with identified pVOIs and their antigenic scores
+#data <- read.csv(paste(output, "/antigenic_scoring_summary_statistics.csv", sep = ""), row.names = 1, sep = ",")
+#data <- data[rowSums(data) >= cutoff,,drop=FALSE]
+#data <- data[1:20,]
+#pvois <- row.names(data)
+
+#sign_lineages_df <- input[input[['antigenic_score']] >= 3.85, ]
+#sign_lineages_df[!duplicated(sign_lineages_df), ]
+
+#pvoi_df <- sign_lineages_df[sign_lineages_df$Pango.lineage %in% pvois, ]
+#pvoi_df_final <- pvoi_df[c("Pango.lineage", "antigenic_score")]
+#print(pvoi_df_final)
+
+#pvoi_df_final %>% datatable() # , options = list(pageLength = 1), rownames = FALSE)
+#saveWidget(pvoi_table, paste(output, "/ntigenic_scoring_pVOItable.html", sep = ""), selfcontained = TRUE)
