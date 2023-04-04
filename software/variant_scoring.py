60b88c553cd001827a31eb4c1d2085506a22c291
knorwood0 <56272255+knorwood0@users.noreply.github.com>
2023-04-04 17:01:12 +0200

    optimized date selection as well as data filtering

--- software/variant_scoring.py
+++ software/variant_scoring.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import os.path
 import sys
 import pandas as pd
 import numpy as np
@@ -8,8 +9,9 @@ import re
 from itertools import chain
 from time import process_time
 from datetime import date
-#from datetime import datetime
 import datetime
+from dateutil.relativedelta import relativedelta
+
 start_time = datetime.datetime.now()
 
 print(sys.argv[0])
@@ -21,7 +23,8 @@ print(sys.argv[5])
 print(sys.argv[6])
 print(len(sys.argv), "\n")
 
-columns = ['Location', 'Collection date', 'Accession ID', 'Pango lineage', 'AA Substitutions']
+print("Reading in the metadata file: ")
+columns = ['Location', 'Collection date', 'Accession ID', 'Pango lineage', 'AA Substitutions', 'Host']
 metadata = pd.read_csv(sys.argv[1], sep = '\t', usecols = columns)
 tpSites = pd.read_csv(sys.argv[2], sep = ',')
 output = sys.argv[3]
@@ -29,59 +32,21 @@ weights = pd.read_csv(sys.argv[4], sep = '\t')
 voc_df = pd.read_csv(sys.argv[5], sep = ',')
 seqsUI = pd.read_csv(sys.argv[6], sep = '\t')
 
-def conversion_position(x):
-    # Function to convert aa list in the metadata file to their position and original / changed aa
-    subsList = x.split(',')
-    mutationsSubsList = []
-    spike_mutations = []
-    for i in subsList:
-        if "Spike" in i:
-            spike_mutations.append(i)
-    for i in spike_mutations:
-        if i.__contains__("_"):
-            pos = i[i.index("_"):]
-            x = pos[1:]
-            pos = "".join([i for i in pos if i.isdigit()])
-            mutationsSubsList.append(pos)
-        else:
-            mutationsSubsList.append(None)
-    return (mutationsSubsList)
+metadata_time = datetime.datetime.now()
+print('import data Duration: {}'.format(metadata_time - start_time))
 
-
-def conversion_originalAA(x):
-    # Function to convert aa list in the metadata file to a list of the original aa
-    subsList = x.split(',')
-    original_aaList = []
-    spike_mutations = []
-    for i in subsList:
-        if "Spike" in i:
-            spike_mutations.append(i)
-    for i in spike_mutations:
-        if i.__contains__("_"):
-            pos = i[i.index("_"):]
-            x = pos[1:]
-            ogAAList = re.findall('([a-zA-Z]*)\d*.*', x)
-            original_aaList.append(ogAAList[0])
-    return (original_aaList)
-
-
-def conversion_changedAA(x):
-    # Function to convert aa list in the metadata file to a list of the original aa
-    subsList = x.split(',')
-    changed_aaList = []
-    spike_mutations = []
-    for i in subsList:
-        if "Spike" in i:
-            spike_mutations.append(i)
-    for i in spike_mutations:
-        if i.__contains__("_"):
-            pos = i[i.index("_"):]
-            x = pos[1:]
-            cAAList = re.findall('\d([a-zA-Z]*)', x)
-            cAA = cAAList[-1:]
-            q = ''.join([str(i) for i in cAA])
-            changed_aaList.append(q)
-    return (changed_aaList)
+def aa_substitution_filter(x, *tp_list):
+    # Funtion to filter out the mutations occuring on the spike protein at known antigenic sites and return separate lists of the positions and mutations
+    remove = ['180','181','182','183','184','185','186','187','188','189','118','218','318','418','518','618','718','818','918','1018','1118','1218']
+    subslist = x.split(',')
+    mutations_list = [i.split("_")[1]for i in subslist if '_' in i and 'Spike' in i] 
+    mutations_list = [i for i in mutations_list if any(x in i for x in tp_list)]
+    #mutations_list = [i for x in tp_list for i in mutations_list if x in i]
+    mutations_list = set(mutations_list)-{i for x in remove for i in mutations_list if x in i} # removing incorrect matches, ie. that contain "18" but are not TP sites
+    subs_positions = [re.findall(r'\d+', i)[0] for i in mutations_list] 
+    originalAA = [re.findall('([a-zA-Z]*)\d*.*', i)[0] for i in mutations_list]
+    changedAA = [re.findall('\d([a-zA-Z]*)', i)[-1] for i in mutations_list]
+    return(subs_positions, originalAA, changedAA)
 
 def seqsUI_filtration(seqsUI, max_month, max_year):
     # Function to return a list of sequences under review for removal prior to analysis
@@ -104,52 +69,24 @@ def month_filtration(metadata, seqsUI_list, max_month, max_year):
     dftemp = dftemp[dftemp['Accession ID'].isin(seqsUI_list) == False]
     return (dftemp)
 
-def mutation_scores(input_df, tpSites_list):  # , output_df
+def mutation_scores(input_df, tpSites_list, weights):
     # Function to add mutation scores to the metadata file based on amino acid changes
     input_df = input_df.dropna(subset=['AA Substitutions'])
 
     # Converting AA Substitutions a list of aa positions for easier comprehension
-    try:
-        print("Converting AA Substitutions and Positions")
-        input_df['AA Substitutions'] = input_df['AA Substitutions'].astype('string')
-        substitutions = input_df['AA Substitutions'].apply(conversion_position)
-        input_df["Substitution_Positions"] = substitutions
-        original_AAs = input_df['AA Substitutions'].apply(conversion_originalAA)
-        input_df["Original_aa"] = original_AAs
-        changed_AAs = input_df['AA Substitutions'].apply(conversion_changedAA)
-        input_df["Changed_aa"] = changed_AAs
-        if len(substitutions) != len(original_AAs):
-            print("Conversion of amino acid list was incorrect and the lengths of the substitution positions defers from the amino acids!")
-            print("Substitution Positions: ", len(substitutions))
-            print("Number of amino acids: ", len(original_AAs))
-            exit()
-    except:
-        print("Unable to parse the AA Substitutions column of the metadata file, please investigate.")
-        exit()
+    print("Converting AA Substitutions and Positions")
+    tp_isolate_mutations = input_df['AA Substitutions'].apply(aa_substitution_filter, args=tpSites_list)
+    input_df['Substitution_Positions'] = [i[0] for i in tp_isolate_mutations]
+    input_df['Original_aa'] = [i[1] for i in tp_isolate_mutations]
+    input_df['Changed_aa'] = [i[2] for i in tp_isolate_mutations]
 
-    try:
-        print("Expanding Substitution Positions")
-        metadata_expanded = input_df.explode(['Substitution_Positions','Original_aa','Changed_aa'])
-        print(pd.DateFrame.head(metadata_expanded))
-        # Calculating the antigenic weight scores dependent on the amino acid changes (from a reference file)
-        print("Filtering based on tp list")
-        metadata_filtered = metadata_expanded[metadata_expanded['Substitution_Positions'].isin(tpSites_list)]
-        print("Merging with the weights file")
-        metadata_filtered_weights = pd.merge(metadata_filtered, weights, how='left', on=['Original_aa', 'Changed_aa'])
-        print("mutation score weight complete")
-    except:
-        print("Unable to expand Substitution_Positions, chunking instead...")
-        metadata_filtered = pd.DataFrame()
-        n = 100
-        t = 0
-        df_chunks = np.array_split(input_df, n) 
-        for chunk in df_chunks:
-            t = t + 1
-            metadata_expanded = chunk.explode(['Substitution_Positions','Original_aa','Changed_aa'])
-            metadata_filtered_chunk = metadata_expanded[metadata_expanded['Substitution_Positions'].isin(tpSites_list)]
-            metadata_filtered = pd.concat([metadata_filtered, metadata_filtered_chunk])
-        metadata_filtered_weights = pd.merge(metadata_filtered, weights, how='left', on=['Original_aa', 'Changed_aa'])
-        print("mutation score weight complete")
+    print("Expanding Substitution Positions")
+    # metadata_expanded = aa_positions_unnest(input_df, ['Original_aa','Changed_aa'])
+    metadata_expanded = input_df.explode(['Substitution_Positions', 'Original_aa', 'Changed_aa'])
+    # Calculating the antigenic weight scores dependent on the amino acid changes (from a reference file)
+    print("Merging with the weights file")
+    metadata_filtered_weights = pd.merge(metadata_expanded, weights, how='left', on=['Original_aa', 'Changed_aa'])
+    print("mutation score weight complete")
 
     return (metadata_filtered_weights)
 
@@ -159,27 +96,25 @@ tpSites_list = tpSites['tp_sites'].values.tolist()
 tpSites_list = [str(item) for item in tpSites_list]
 print("Done\n")
 
-# Calculating Antigenic Scores by chunking Metadata
+tplist_time = datetime.datetime.now()
+print('tp site list Duration: {}'.format(tplist_time - metadata_time))
+
+# Filtering by most recenet month by chunking the Metadata file to reduce computational load
 print("Calculating Mutation Scores....")
 print("Getting most recent month")
 if len(sys.argv) == 9:  
     print("Using user input month and year settings")
     # Using a predetermined month and year for the analysis that the user inputs
-    max_month = str(sys.argv[6])  # add one back
-    max_year = str(sys.argv[7])  # add one back
+    max_month = str(sys.argv[6])  
+    max_year = str(sys.argv[7])  
 
 elif len(sys.argv) == 7: 
     # Calculating the most recent whole month and year based on today's date
     today = date.today()
-    firstOfMonth = today.replace(day=1)
-    lastMonth = firstOfMonth - datetime.timedelta(days=1)
-    max_month = str(lastMonth.month)
-    # Calculating the year, needs to change if the previous month was December
-    if max_month == "12":
-        lastYear = firstOfMonth - datetime.timedelta(days=366)
-        max_year = str(lastYear) 
-    else:
-        max_year = str(today.year)
+    lastMonth = (today - relativedelta(months = 1)).month
+    year = (today - relativedelta(months = 1)).year
+    max_month = str(lastMonth)
+    max_year = str(year)
     if len(max_month) < 2:
         max_month = "0" + str(max_month)
 
@@ -191,49 +126,80 @@ else:
     print("Wrong number of arguments, please use -h for more information")
     sys.exit(1)
     
-max_year = str(max_year) #New line
-max_month = str(max_month) # New line
+max_year = str(max_year)
+max_month = str(max_month)
+
+month_calc_time = datetime.datetime.now()
+print('month calculation Duration: {}'.format(month_calc_time - tplist_time))
 
 print("Filtering Metadata by most recent month")
 n = len(metadata) // 50  # 1500
+t=0
 print("n: ", n)
 
 monthly_metadata = pd.DataFrame()
 seqsUI_list = seqsUI_filtration(seqsUI, max_month, max_year)
 df_chunks = np.array_split(metadata, n)
 for chunk in df_chunks:
+    t = t + 1
     dfMonth_chunk = month_filtration(chunk, seqsUI_list, max_month, max_year)
     monthly_metadata = pd.concat([monthly_metadata, dfMonth_chunk], axis = 0)
 print("Monthly Metadata 1st Attempt: ")
 print("Length of monthly_metadata: ", len(monthly_metadata))
-print("Column names: ", monthly_metadata.columns.values)
 print(monthly_metadata.head())
 
+# Fail safe to go back one month if metadata file empty for current month
 if len(monthly_metadata) == 0:
     print("No isolate data for: ", max_month, "-", max_year)
-    max_month = int(max_month) - 1
-    if max_month == 0:
-        max_month = "12"
-    else:
-        max_month = str(max_month)
-        if len(max_month) < 2:
-            max_month = "0" + str(max_month)
-    print("Now using data from: ", max_month, "-", max_year)
+    twoMonths_ago = (today - relativedelta(months = 2)).month
+    twoMonths_year = (today - relativedelta(months = 2)).year
+    max_month = str(twoMonths_ago)
+    max_year = str(twoMonths_year)
+    if len(max_month) < 2:
+        max_month = "0" + str(max_month)
+    print("Now trying data from: ", max_month, "-", max_year)
 
     monthly_metadata = pd.DataFrame()
     seqsUI_list = seqsUI_filtration(seqsUI, max_month, max_year)
     for chunk in df_chunks:
         dfMonth_chunk = month_filtration(chunk, seqsUI_list, max_month, max_year)
         monthly_metadata = pd.concat([monthly_metadata, dfMonth_chunk], axis = 0)
+
+# Going back two months if data still empty
+if len(monthly_metadata) == 0:
+    print("No isolate data for: ", max_month, "-", max_year)
+    threeMonths_ago = (today - relativedelta(months = 3)).month
+    threeMonths_year = (today - relativedelta(months = 3)).year
+    max_month = str(threeMonths_ago)
+    max_year = str(threeMonths_year)
+    if len(max_month) < 2:
+        max_month = "0" + str(max_month)
+    print("Now trying data from: ", max_month, "-", max_year)
+
+    monthly_metadata = pd.DataFrame()
+    seqsUI_list = seqsUI_filtration(seqsUI, max_month, max_year)
+    for chunk in df_chunks:
+        dfMonth_chunk = month_filtration(chunk, seqsUI_list, max_month, max_year)
+        monthly_metadata = pd.concat([monthly_metadata, dfMonth_chunk], axis = 0)
+
+# Throwing error if data from two months ago is still empty
 if len(monthly_metadata) == 0:
     print("ERROR - despite using data from two months ago, the monthly_metadata file is still empty, please check!")
     exit()
 print("Monthly Filtration Complete\n")
+month_filter_time = datetime.datetime.now()
+print('monthly filtration Duration: {}'.format(month_filter_time - month_calc_time))
 
 print("Writing Months Text File")
 month_file = open(output + "month_vis.txt", "w")
 month_file.writelines(str(max_month) + "-" + str(max_year))
 month_file.close()
+
+month_file_time = datetime.datetime.now()
+print('month file Duration: {}'.format(month_file_time - month_filter_time))
+
+# Filtering by "Human" host data only
+monthly_metadata = monthly_metadata.loc[monthly_metadata['Host'] == 'Human']
 
 print("\nRunning Analysis - Calculating Antigenic Scores for Pango Lineages")
 metadata_filtered_weights = pd.DataFrame()
@@ -244,25 +210,22 @@ t = 0
 df_chunks = np.array_split(monthly_metadata, n)  # (metadata, n)
 tstart = process_time()
 for chunk in df_chunks:
-    print(chunk.shape[0])
     t = t + 1
     print(t)
-
-    if len(chunk) == 0:
-        print("Empty df after filtering")
-        continue
-    else:
-        # Calculating Weights
-        mutation_df = mutation_scores(chunk, tpSites_list)
-        # Agreggating by accession id
-        mutation_df = mutation_df.groupby(mutation_df['Accession ID']).aggregate({'Weight': 'sum'})
-        metadata_filtered_weights = pd.concat([mutation_df, metadata_filtered_weights])
+    # Calculating Weights
+    mutation_df = mutation_scores(chunk, tpSites_list, weights)
+    # Agreggating by accession id
+    mutation_df = mutation_df.groupby(mutation_df['Accession ID']).aggregate({'Weight': 'sum'})
+    metadata_filtered_weights = pd.concat([mutation_df, metadata_filtered_weights])
 
     print("Output DataFrame Shape: ")
     print(metadata_filtered_weights.shape)
     print("Chunk complete")
 tstop = process_time()
 print("\nProcess Time: ", tstop - tstart)
+
+analysis_time = datetime.datetime.now()
+print('analysis time Duration: {}'.format(analysis_time - month_file_time))
 
 # Summing Weights by accession ID
 print("Merging Dataframes")
@@ -276,6 +239,9 @@ df_final = df[["Accession ID", "Collection date", "Location", "Pango lineage", "
 
 print("Saving dataframe...")
 df_final.to_csv(output + "antigenic_scores_all.csv", sep='\t', index=False, header=True)
+
+df_final_time = datetime.datetime.now()
+print('df_final Duration: {}'.format(df_final_time - analysis_time))
 
 # Ranking pango lineages based on average mutation score across all the sequences
 df_ranked = df_final[["Pango lineage", "antigenic_score"]]
