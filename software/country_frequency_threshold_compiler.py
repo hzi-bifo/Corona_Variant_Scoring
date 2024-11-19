b1500e6609e84a0933e59b82d08b0261ea3e9452
knorwood0 <56272255+knorwood0@users.noreply.github.com>
2024-11-19 14:23:16 +0100

    added lineage information column to cumulative file

--- software/country_frequency_threshold_compiler.py
+++ software/country_frequency_threshold_compiler.py
@@ -3,7 +3,7 @@
 
 # ## Country Frequency Threshold
 # #### Author: Katrina Norwood
-# #### Last Updated: 18/11/2024
+# #### Last Updated: 19/11/2024
 # 
 # Script to identify which countries have at least 1% of the monthly sequences (mostly for the earlier months ie. Jan 2020) or have 500 sequences. These countries will then be used for the country_score_comparison_over_time analysis. 
 
@@ -37,8 +37,9 @@ else:
 
 # Parsing through the month directories to find countries that had at least 1% of sequences
 print("Reading In Input DFs: ")
-antigenic_score_df = pd.read_csv(monthlyComparison_dir+"/antigenic_scores_all.csv", sep = '\t', usecols = ["Location"])
+antigenic_score_df = pd.read_csv(monthlyComparison_dir+"/antigenic_scores_all.csv", sep = '\t', usecols = ["Location", "Pango lineage"])
 country_score_df = pd.read_csv(monthlyComparison_dir+"/antigenic_scores_map_visualization.csv", sep = "\t", usecols = ["Country", "country_score"])
+zscore_df = pd.read_csv(monthlyComparison_dir+"/antigenic_scores_ranked_with_WHO.csv", sep = "\t", usecols = ["Pango lineage", "antigenic_score", "zscore"])
 
 # Checking to see which countries have a frequency of greater than 1% of the global submitted sequences
 print("Adding number of seqeunces and parsing out country information: ")
@@ -53,23 +54,69 @@ for item in antigenic_score_df["Country"]:
     y = x.rstrip()
     country.append(y)
 antigenic_score_df["Country"] = country
-        
-# Getting number of isolates and their frequency per country
-antigenic_score_countries_df = antigenic_score_df.value_counts(['Country']).reset_index().rename(columns={0:'num_isolates'})
-antigenic_score_countries_df["seq_frequency"] = antigenic_score_countries_df["num_isolates"] / num_sequences
+print(pd.DataFrame.head(antigenic_score_df))
+
+# Getting number of isolates and their frequency per country (to identify countries that match the threshold)
+print("Getting number of isolates per country: ")
+antigenic_score_df["num_isolates"] = antigenic_score_df.groupby("Country")["Country"].transform("count")
+antigenic_score_df["seq_frequency"] = antigenic_score_df["num_isolates"] / num_sequences
+
+# Calculating frequency per pango lineage 
+print("Calculating lineage frequency: ")
+antigenic_score_df["num_lineage_isolates"] = antigenic_score_df.groupby(['Country', 'Pango lineage'])['Pango lineage'].transform("count")
+antigenic_score_df["lineage_frequency"] = antigenic_score_df["num_lineage_isolates"] / antigenic_score_df["num_isolates"]
+print("After adding lineage and lineage frequency: ")
+print(pd.DataFrame.head(antigenic_score_df))
+
+# Merging antigenic score and zscore
+print("Merging antigenic score and zscore: ")
+antigenic_score_countries_df = antigenic_score_df.merge(zscore_df, how='left', on='Pango lineage')
+print(pd.DataFrame.head(antigenic_score_countries_df))
 
 # Creating a list of the countries with greater than 1% representation
 print("Creating list of countries that meet threshold: ")
-freq_countries_monthly_df = antigenic_score_countries_df[(antigenic_score_countries_df['seq_frequency'] >= 0.01) | (antigenic_score_countries_df['num_isolates'] >= 500)]
+freq_countries_monthly_df = antigenic_score_countries_df.drop(['Location'], axis = 1)
+freq_countries_monthly_df = freq_countries_monthly_df[(freq_countries_monthly_df['seq_frequency'] >= 0.01) | (freq_countries_monthly_df['num_isolates'] >= 500)]
+
+# Dropping duplicates
+print("Dropping duplicates: ")
+freq_countries_monthly_df = freq_countries_monthly_df.drop_duplicates()
+print(pd.DataFrame.head(freq_countries_monthly_df, n = 25))
+
+# Keeping lineages with the highest frequency per country (country score = antigenic score x frequency)
+print("Keeping most frequent lineages per country: ")
+freq_countries_monthly_df = freq_countries_monthly_df.groupby('Country', group_keys=False).apply(
+    lambda x: x.nlargest(5, 'lineage_frequency')
+).reset_index(drop=True)
+# Cleaning up decimal places
+freq_countries_monthly_df["lineage_frequency"] = freq_countries_monthly_df["lineage_frequency"].round(3)
+freq_countries_monthly_df["antigenic_score"] = freq_countries_monthly_df["antigenic_score"].round(3)
+freq_countries_monthly_df["zscore"] = freq_countries_monthly_df["zscore"].round(3)
+print(pd.DataFrame.head(freq_countries_monthly_df, n = 25))
+
+# Putting together the lineage information
+print("Putting together lineage information per country: ")
+freq_countries_monthly_df['lineages_information'] = freq_countries_monthly_df.apply(lambda row: {
+    'Pango lineage': row['Pango lineage'],
+    'lineage_frequency': row['lineage_frequency'],
+    'antigenic_score': row['antigenic_score'],
+    'zscore': row['zscore']}, axis=1)
+print(pd.DataFrame.head(freq_countries_monthly_df))
+lineage_information = freq_countries_monthly_df.groupby('Country')['lineages_information'].apply(list).reset_index()
+print(lineage_information)
+# Removing redundant columns
+freq_countries_monthly_df = freq_countries_monthly_df.drop(['Pango lineage', 'lineage_frequency', 'zscore', 'num_lineage_isolates', 'antigenic_score', 'lineages_information'], axis = 1)
+# Dropping duplicates (so theres a dataframe of country and country info only)
+freq_countries_monthly_df = freq_countries_monthly_df.drop_duplicates()
+# Adding dictionary of information to df
+freq_countries_monthly_df = freq_countries_monthly_df.merge(lineage_information, how = "left", on = "Country")
+print(pd.DataFrame.head(freq_countries_monthly_df))
 
 # Creating a df with the high freq countries and their country antigenic score for that month
-print("Adding date column and dropping duplicates: ")
+print("Adding date column: ")
 # Adding date column
 print(pd.DataFrame.head(freq_countries_monthly_df))
 freq_countries_monthly_df['date'] = str(month)
-# Dropping duplicates
-print("dropping duplicates")
-freq_countries_monthly_df = freq_countries_monthly_df.drop_duplicates()
 
 # Checking to see if the month has already been added to the cumulative file, and if not, adding it
 print("Adding new countries to the cumulative file: ")
@@ -79,3 +126,5 @@ if not (df_cumulative['date'].eq(month)).any():
 else:
     print("Already added month, passing...")
     pass
+
+freq_countries_monthly_df.to_csv(reference_dir + "country_list_with_threshold_test.tsv", sep='\t', index=False, header=True)
